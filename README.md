2348d87e82e27801427520968d3990f1d8729cc3
Maximilian Held <info@maxheld.de>
2017-10-25 18:54:16 +0200

    set up boilerplate, start on #1

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Build Status](https://travis-ci.com/QWrks/sublab.svg?token=VqJKxxUwxXFJyYyqJx7G&branch=master)](https://travis-ci.com/QWrks/sublab)
+
+# worktypes: dimensionality reduction of BIBB/BAUA data
+
+*[Sabine Pfeiffer](https://www.sabine-pfeiffer.de), [Maximilian Held](http://www.maxheld.de)*
---
+++ setup.R
@@ -0,0 +1,9 @@
+# dependencies ====
+library(bookdown)
+library(knitr)
+
+# knitr setup ====
+#opts_knit$set(root.dir = normalizePath(getwd()))  # make sure the knitr path is correct
+knitr::opts_chunk$set(tidy = TRUE, cache = TRUE, echo = FALSE, message = FALSE)  # tidy formats code nicely in echo
+options(digits = 2)  # display only 2 digits in knitr output
+options(scipen = 999)
