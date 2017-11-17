43b3a7b3633ff5f55f07f6381df888c4ce6f3831
Maximilian Held <info@maxheld.de>
2017-11-17 18:22:42 +0100

    improve plots, minor issues

--- setup.R
+++ setup.R
@@ -11,3 +11,4 @@ library(tidyverse)
 knitr::opts_chunk$set(tidy = TRUE, cache = TRUE, echo = FALSE, message = FALSE)  # tidy formats code nicely in echo
 options(digits = 2)  # display only 2 digits in knitr output
 options(scipen = 999)
+Sys.setenv(TZ="Europe/Berlin")
